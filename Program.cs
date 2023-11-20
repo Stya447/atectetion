10582bf744819fe109a0afcb8093eaa3938f7525
Anastasia Akhmetova <asyaphilip447@gmail.com>
2023-11-20 16:07:07 +0300

    Решение 2 задачи

--- Program.cs
+++ Program.cs
@@ -20,42 +20,42 @@
 //M = 1; N = 15 -> 120
 //M = 4; N = 8. -> 30
 
-//int SumNumbers (int m, int n)
-//{
-//    if (n == 0 || m == 0) return 0;
-//    if (n > m)
-//    {       
-//        return SumNumbers(n - 1, m) + n; 
-//    }
-//    if (n < m)
-//    {
-//        return SumNumbers(n + 1, m) + n;
-//    }
-//    if (n==m)
-//    {
-//        return m;
-//    }
-//    else return n;
-//}
-//System.Console.Write(SumNumbers(4,8)+ " ");
+int SumNumbers (int m, int n)
+{
+    if (n == 0 || m == 0) return 0;
+    if (n > m)
+    {       
+        return SumNumbers(n - 1, m) + n; 
+    }
+    if (n < m)
+    {
+        return SumNumbers(n + 1, m) + n;
+    }
+    if (n==m)
+    {
+        return m;
+    }
+    else return n;
+}
+System.Console.Write(SumNumbers(4,8)+ " ");
 
 //Напишите программу вычисления   с помощью рекурсии. Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(m,n) = 9
 //m = 3, n = 2 -> A(m,n) = 29
 
-int Akkerman(int m, int n)
-{
-    if (m == 0)
-    {
-        return n + 1;
-    }
-    else if (n == 0 && m > 0)
-    {
-        return Akkerman(m - 1, 1);
-    }
-    else
-    {
-        return (Akkerman(m - 1, Akkerman(m, n - 1)));
-    }
-}
-System.Console.Write(Akkerman(3,2)+ " ");
+//int Akkerman(int m, int n)
+//{
+//    if (m == 0)
+//    {
+//        return n + 1;
+//    }
+//    else if (n == 0 && m > 0)
+//    {
+//        return Akkerman(m - 1, 1);
+//    }
+//    else
+//    {
+//        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//    }
+//}
+//System.Console.Write(Akkerman(3,2)+ " ");
